649bdce9d3acc43c0fea47bec3749b95127e7445
gibba42 <alastairgibb.kent@gmail.com>
2026-02-09 22:58:46 +0000

    Replaced base code with example from trinket.io python battleship example. Made minor changes and added comments to lines that will be edited in future commits.

--- run.py
+++ run.py
@@ -1,47 +1,58 @@
-"""
-Import the random module to generate random numbers.
-Used to allow the computer to place ships.
-"""
+# Import random to allow the computer to randomly place ships
+from random import randint
 
-import random 
+board = []
 
-def create_board(size):
-    """
-    Creates a square game board.
-    Each cell is set to '~' to represent water.
-    """
-    return [['~'] * size for _ in range(size)]
+"""
+Sets the board to 6 X 6. 
+Sets the cells to '~' for water.
+Will change this later to allow the user to set the size.
+"""
+for x in range(6):
+    board.append(["~"] * 6)
 
 def print_board(board):
-    """
-    Prints the game board to the console. 
-    Each row is printed on a new line. 
-    """
     for row in board:
-        print(' '.join(row))
-    print()
+        print((" ").join(row))
 
-def place_ships(board, num_ships):
-    """
-    Randomly places a given number of ships on the board.
-    Ships are represented by 'S'.
-    Ensures ships cannot overlap.
-    """
-    ships = 0
-    while ships < num_ships:
-        x, y = random.randint(0, len(board) - 1), random.randint(0, len(board) - 1)
-        if board[x][y] == '~':
-            board[x][y] = 'S'
-            ships += 1
-    
-def get_user_guess():
-    """
-    Asks the user to enter a row and column.
-    Repeats until a valid input is
-    """
-    while True:
-        guess = input("Enter your guess (row and column, e.g. 2 3): ").split()
-        if len(guess) == 2 and guess[0].isdigit() and guess[1].isdigit():
-            return int(guess[0]), int(guess[1])
+# Game start message
+print("Let's play Battleship!")
+print_board(board)
+
+"""
+Randomly places ships.
+Might change this later so that ships can be multiple cells long.
+"""
+def random_row(board):
+    return randint(0, len(board) - 1)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+# Allows the user 9 guesses
+for turn in range(9):
+    print ("Turn"), turn
+    guess_row = int(input("Guess Row:")) # Asks user to guess a row
+    guess_col = int(input("Guess Col:")) #Asks user to guess a column
+    # Might change this to ask in one go, this is clunky
+
+    # Checks if user guessed a cell with a ship in it
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sunk my battleship!")
+        break
+    else:
+        # Checks if guess was on the board. Will change this so that users cannot make invalid guesses.
+        if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
+            print("Oops, that's not even in the ocean.")
+        elif(board[guess_row][guess_col] == "X"):
+            # Checks if guess was on a 'hit' cell
+            print("You guessed that one already.")
         else:
-            print("Invalid input. Please enter two numbers separated by a space.")
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+    if turn == 8:
+        print("Game Over")
+    turn =+ 1
+    print_board(board)
