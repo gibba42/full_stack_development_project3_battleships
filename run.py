7fb2d96f9eb9c1bf3a068917ce58b49b26e10850
gibba42 <alastairgibb.kent@gmail.com>
2026-02-10 21:02:19 +0000

    Restarted run.py. Added the print_board function that draws the game board.

--- run.py
+++ run.py
@@ -1,56 +1,12 @@
-# Import random to allow the computer to randomly place ships
 from random import randint
 
 board = []
 
-"""
-Sets the board to 6 X 6. 
-Sets the cells to '~' for water.
-Will change this later to allow the user to set the size.
-"""
 for x in range(6):
     board.append(["~"] * 6)
 
 def print_board(board):
-    letters = "A B C D E F" # Print a letter for each column
-    print(" " + letters)
-    for i, row in enumerate(board): # Print a number on each row
-        print(str(i + 1) + " " + " ".join(row))
+    for row in board:
+        print(" ".join(row))
 
-"""
-Randomly places ships.
-Might change this later so that ships can be multiple cells long.
-"""
-def random_row(board):
-    return randint(0, len(board) - 1)
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-
-# Allows the user 9 guesses
-for turn in range(9):
-    print ("Turn"), turn
-    guess_row = int(input("Guess Row:")) # Asks user to guess a row
-    guess_col = int(input("Guess Col:")) #Asks user to guess a column
-    # Might change this to ask in one go, this is clunky
-
-    # Checks if user guessed a cell with a ship in it
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship!")
-        break
-    else:
-        # Checks if guess was on the board. Will change this so that users cannot make invalid guesses.
-        if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
-            print("Oops, that's not even in the ocean.")
-        elif(board[guess_row][guess_col] == "X"):
-            # Checks if guess was on a 'hit' cell
-            print("You guessed that one already.")
-        else:
-            print("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
-    if turn == 8:
-        print("Game Over")
-    turn =+ 1
-    print_board(board)
+print_board(board)
