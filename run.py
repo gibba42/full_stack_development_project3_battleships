7352ff3c4846bd92603c794b08e271b05a5bf338
gibba42 <alastairgibb.kent@gmail.com>
2026-02-10 21:48:20 +0000

    Updated the print_board function to add column and row headers. This should also help debug the guess validation issue.

--- run.py
+++ run.py
@@ -6,8 +6,12 @@ for x in range(6):
     board.append(["~"] * 6)
 
 def print_board(board):
-    for row in board:
-        print(" ".join(row))
+    # Print column headers
+    print("  A B C D E F")
+
+    # Print each row with a row number
+    for i, row in enumerate(board, start=1):
+        print(f"{i} " + " ".join(row))
 
 def random_row(board):
     return randint(0, len(board[0]) - 1)
@@ -32,13 +36,12 @@ def game():
             print("Congratulations! You sunk the battleship!")
             break
         else:
-            if (guess_row < 0 or guess_row > 6) or (guess_col < 0 or guess_col > 6):
-                print("Please enter a guess between 0 and 5.")
+            if (guess_row < 1 or guess_row > 6) or (guess_col < 1 or guess_col > 6):
+                print("Please enter a guess between 1 and 6.")
 
             else:
                 print("Miss!")
                 board[guess_row][guess_col] = "O"
-                print("Turn ") + turn
         print_board(board)
 
 game()
